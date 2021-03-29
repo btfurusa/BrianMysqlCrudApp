a04e941001e7abd8986e13910d696a241e405803
btfurusa <briantfurusa0@gmail.com>
2021-03-29 22:01:32 +0200

    monday night

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -7,8 +7,10 @@ router.get('/', employeeController.findAll);
 router.post('/', employeeController.create);
 // Retrieve a single employee with id
 router.get('/:id', employeeController.findById);
+
 // Update a employee with id
 router.put('/:id', employeeController.update);
 // Delete a employee with id
+
 router.delete('/:id', employeeController.delete);
 module.exports = router
